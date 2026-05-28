619909a202ad7f567d1675716f2a58c0a0946fe5
LucianoMazar <lmazaraojr@gmail.com>
2026-05-28 10:23:08 -0300

    feat(frontend): add responsiveness to dashboard

--- dedg-front/src/app/shared/ui/header/header.ts
+++ dedg-front/src/app/shared/ui/header/header.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { LucideLogOut } from '@lucide/angular';
+import { Component, inject } from '@angular/core';
+import { LucideLogOut, LucideMenu } from '@lucide/angular';
+import { LayoutService } from '../../layout.service';
 
 @Component({
   selector: 'app-header',
-  imports: [LucideLogOut],
+  imports: [LucideLogOut, LucideMenu],
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  layout = inject(LayoutService);
+}
--- dedg-front/src/app/shared/ui/side-bar/side-bar.ts
+++ dedg-front/src/app/shared/ui/side-bar/side-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import {
   LucideBuilding2,
@@ -6,14 +6,18 @@ import {
   LucideHistory,
   LucideUsers,
   LucideCalendar,
+  LucideX,
 } from '@lucide/angular';
+import { LayoutService } from '../../layout.service';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [RouterLink, RouterLinkActive, LucideBuilding2, LucideLayoutDashboard, LucideHistory, LucideUsers, LucideCalendar],
+  imports: [RouterLink, RouterLinkActive, LucideBuilding2, LucideLayoutDashboard, LucideHistory, LucideUsers, LucideCalendar, LucideX],
   templateUrl: './side-bar.html',
 })
 export class SideBar {
+  layout = inject(LayoutService);
+
   // TODO: substituir por dados do usuário autenticado
   user = {
     name: 'Ricardo Silva',
