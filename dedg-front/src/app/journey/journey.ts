b8efc1bfe7a0a3915faab4bcf04648e0eed6d1d4
LucianoMazar <lmazaraojr@gmail.com>
2026-05-27 21:15:21 -0300

    chore(frontend): planning the project structure

---
+++ dedg-front/src/app/journey/journey.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journey',
+  imports: [],
+  templateUrl: './journey.html',
+})
+export class Journey {}
---
+++ dedg-front/src/app/journey/period/period.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-period',
+  imports: [],
+  templateUrl: './period.html',
+})
+export class Period {}
