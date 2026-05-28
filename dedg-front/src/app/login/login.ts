95951975fc079118d81006a8c4a4bf6374c4b47e
LucianoMazar <lmazaraojr@gmail.com>
2026-05-28 08:22:30 -0300

    feat(frontend): add login page layout

--- dedg-front/src/app/login/login.ts
+++ dedg-front/src/app/login/login.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  LucideBuilding2,
+  LucideMail,
+  LucideLock,
+  LucideEye,
+  LucideEyeOff,
+  LucideArrowRight,
+} from '@lucide/angular';
+import { Footer } from "../shared/ui/footer/footer";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, LucideBuilding2, LucideMail, LucideLock, LucideEye, LucideEyeOff, LucideArrowRight, Footer],
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+  rememberMe = false;
+  showPassword = signal(false);
+
+  togglePassword() {
+    this.showPassword.update((v) => !v);
+  }
+
+  onSubmit() {
+    // execução teste para o botão
+    console.log({ email: this.email, rememberMe: this.rememberMe });
+  }
+}
